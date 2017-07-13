3c320c8e4ac93b2c56cc6ffe6800d1c4b73ef5bb
马燕 <mosic.ma@hotmail.com>
2017-07-13 23:56:10 +0800

    update

--- index.js
+++ index.js
@@ -1,19 +1,67 @@
 /**
  * Created by mayan on 2017/7/13.
  */
-(function () {
-    var sliderInterval = setInterval(play(),3000);
-    var sliderClock = setInterval(clock(),2000);
-    var i = 1;
+
+
+window.onload = (function() {
+    var $ = function (id) {
+        return document.getElementById(id);
+    };
+    var box = $('sliderImgBox');
+    var btnLeft = $('sliderLeft');
+    var btnRight = $('sliderRight');
+    var dot = $('dotNav').getElementsByTagName('a');
+    var index = 1;
+
+
+    //自动轮播
     function play() {
-        var box = document.getElementsByClassName('slider-img-box')[0];
-        box.style.marginLeft = (1-i)*1180+'px';
+        box.style.left = (1-index)*1180+'px';
     }
     function clock() {
-        i = i+1;
-        if(i===5){
-            i = 1;
+        index = index+1;
+        if(index===5){
+            index = 1;
         }
     }
+    var slide = setInterval(play(),3000);
+    var time = setInterval(clock(),2000);
+
+
+    //点击dot切换图片
+    for(i<dot.length+1; i++){
+        dot[i].onclick = function () {
+            var index = Number(this.href.replace(/.*#/g, "")) || 1;
+            box.style.left = (1 - index) * 1180 + "px";
+            // dot[i].addClass('active');
+            return false;
+        };
+    }
+
+
+    //点击左右按钮切换图片
+    btnLeft.onclick = function () {
+        if(index = 5){
+            index = 1;
+        }else{
+            index += 1;
+        }
+        play()
+    }
+    btnRight.onclick = function () {
+        if(index = 1){
+            index = 5;
+        }else{
+            index -= 1;
+        }
+        play()
+    }
+
+
+
+
 })();
+
+
+
 
