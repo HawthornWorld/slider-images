4a04773dcd8e63bb98edca0f6c5085a3f138d3b9
马燕 <mosic.ma@hotmail.com>
2017-07-15 00:06:29 +0800

    update

--- index.js
+++ index.js
@@ -7,60 +7,130 @@ window.onload = (function() {
     var $ = function (id) {
         return document.getElementById(id);
     };
+    var wrapper = document.getElementsByClassName('slider-img-wrapper')[0];
     var box = $('sliderImgBox');
     var btnLeft = $('sliderLeft');
     var btnRight = $('sliderRight');
     var dot = $('dotNav').getElementsByTagName('a');
+    var animated = false;
     var index = 1;
+    var len = 5;
+    var interval = 3000;
+    var timer;
 
-
-    //自动轮播
-    function play() {
-        box.style.left = (1-index)*1180+'px';
-    }
-    function clock() {
-        index = index+1;
-        if(index===5){
-            index = 1;
+    //滑动动画
+    function animate(offset) {
+        if (offset === 0) {
+            return;
         }
-    }
-    var slide = setInterval(play(),3000);
-    var time = setInterval(clock(),2000);
-
-
-    //点击dot切换图片
-    for(i<dot.length+1; i++){
-        dot[i].onclick = function () {
-            var index = Number(this.href.replace(/.*#/g, "")) || 1;
-            box.style.left = (1 - index) * 1180 + "px";
-            // dot[i].addClass('active');
-            return false;
+        animated = true;
+        var time = 300;
+        var inteval = 10;
+        var speed = offset / (time / inteval);
+        var marginLeft = parseInt(box.style.marginLeft) + offset;
+        var go = function () {
+            if (speed > 0 && parseInt(box.style.marginLeft < marginLeft) || speed < 0 && parseInt(box.style.marginLeft > marginLeft)) {
+                box.style.marginLeft = parseInt(box.style.marginLeft) + speed + 'px';
+                setTimeout(go, inteval);
+            }
+            else {
+                box.style.marginLeft = marginLeft + 'px';
+                if (marginLeft > -1180) {
+                    box.style.marginLeft = -1180 * len + 'px';
+                }
+                if (marginLeft < (-1180 * len)) {
+                    box.style.marginLeft = '-1180px';
+                }
+                animated = false;
+            }
+            go();
         };
-    }
+        // animate();
 
-
-    //点击左右按钮切换图片
-    btnLeft.onclick = function () {
-        if(index = 5){
-            index = 1;
-        }else{
-            index += 1;
+        //切换时拉长小圆点
+        function translateDot() {
+            for (var i = 0; i < dot.length; i++) {
+                if (dot[i].className === 'on') {
+                    dot[i].className = 'active';
+                    break;
+                }
+            }
+            dot[index - 1].className = 'active';
         }
-        play()
-    }
-    btnRight.onclick = function () {
-        if(index = 1){
-            index = 5;
-        }else{
-            index -= 1;
+
+        //自动轮播
+        function play() {
+            timer = setTimeout(function () {
+                btnRight.onclick();
+                play();
+            }, interval);
         }
-        play()
+
+        //停止轮播
+        function stop() {
+            clearTimeout(timer);
+        }
+
+        //点击右箭头，向左移动
+        btnRight.onclick = function () {
+            if (animated) {
+                return;
+            }
+            if (index === 5) {
+                index = 1;
+            } else {
+                index += 1;
+            }
+            translateDot();
+            if (!animated) {
+                this.animate(-1180);
+            }
+        };
+
+        //点击左箭头，向右移动
+        btnLeft.onclick = function () {
+            if (animated) {
+                return;
+            }
+            if (index === 1) {
+                index = 5;
+            } else {
+                index -= 1;
+            }
+            translateDot();
+            if (!animated) {
+                animate(-1180);
+            }
+        };
+
+        //点击圆点触发事件
+        for (var i = 0; i < dot.length; i++) {
+            dot[i].onclick = function () {
+                if (animated) {
+                    return;
+                }
+                if (this.className === 'active') {
+                    return;
+                }
+                var currIndex = parseInt(this.getAttribute('index'));
+                var offset = -1180 * (currIndex - index);
+                if (!animated) {
+                    animate(offset);
+                }
+                index = currIndex;
+                translateDot();
+            }
+        }
+        wrapper.onmouseover = stop;
+        wrapper.onmouseout = play;
+        play();
     }
+});
 
 
 
 
-})();
+
 
 
 
---
+++ loop.js
@@ -0,0 +1,6 @@
+/**
+ * Created by mayan on 2017/7/14.
+ */
+(function () {
+
+})();
